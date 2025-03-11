f25648a72688409b7c23ef681366f879e1712a85
BoubakarPI <agenceweb1919@gmail.com>
2025-03-11 22:12:23 +0000

    feat: add BankAccount setter & getter

--- src/main/java/com/uvatis/Main.java
+++ src/main/java/com/uvatis/Main.java
@@ -1,7 +1,12 @@
 package com.uvatis;
 
+import com.uvatis.model.BankAccount;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        BankAccount account1 = new BankAccount();
+        account1.setBalance(152);
+        System.out.println(account1.getBalance());
     }
 }
--- src/main/java/com/uvatis/model/BankAccount.java
+++ src/main/java/com/uvatis/model/BankAccount.java
@@ -5,4 +5,32 @@ public class BankAccount {
     protected String status;
     protected double balance;
     private String currency;
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
